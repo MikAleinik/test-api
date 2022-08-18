7c462be59c4a969e5fc0cd0e178fb67d7c67545a
Mikhail Aleinik <84644126+MikAleinik@users.noreply.github.com>
2022-08-18 17:17:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n');
-}).listen(10000, '0.0.0.0');
-console.log('Server running at http://0.0.0.0:10000/');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080);
